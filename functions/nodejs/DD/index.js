0815bcf7f673beaa0b7b263b5f2d3983139a3307
1786248714123444.feishu.cn <1786248714123444@feishu.cn>
2023-12-26 07:57:16 +0000

    3 个文件产生了变更，影响了1 个 函数
    增加函数「DD」

---
+++ functions/nodejs/DD/index.js
@@ -0,0 +1,33 @@
+// 通过 NPM dependencies 成功安装 NPM 包后此处可引入使用
+// 如安装 linq 包后就可以引入并使用这个包
+// const linq = require("linq");
+
+/**
+ * @param {Params}  params     自定义参数
+ * @param {Context} context    上下文参数，可通过此参数下钻获取上下文变量信息等
+ * @param {Logger}  logger     日志记录器
+ *
+ * @return 函数的返回数据
+ */
+module.exports = async function (params, context, logger) {
+  // 日志功能
+  // logger.info(`${new Date()} 函数开始执行`);
+
+  // 在这里补充业务代码
+  function myFunction(param) {
+    // 入参处理
+    console.log("入参：" + param);
+   
+    // 执行一些操作
+   
+    // 出参处理
+    var result = "出参：" + param;
+    console.log(result);
+    return result;
+  }
+   
+  // 调用函数并传入参数
+  var input = "Hello World";
+  var output = myFunction(input);
+  console.log(output);
+}
