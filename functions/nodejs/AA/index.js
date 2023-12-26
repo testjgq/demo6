a1086e1f4c88678556c7e00b76026ec4219503b0
1786248714123444.feishu.cn <1786248714123444@feishu.cn>
2023-12-26 08:53:14 +0000

    1 个文件产生了变更，影响了1 个 函数
    更新函数「AA」

--- functions/nodejs/AA/index.js
+++ functions/nodejs/AA/index.js
@@ -16,20 +16,21 @@ module.exports = async function (params, context, logger) {
   // 在这里补充业务代码
   logger.info("AA");
   logger.info("AAA");
-  function AA(param) {
+  function AA(num1, num2) {
     // 入参处理
-    console.log("入参：" + param);
-   
+    console.log("入参1：" + num1);
+    console.log("入参2：" + num2);
+  
     // 执行一些操作
-   
+    var sum = num1 + num2;
+  
     // 出参处理
-    var result = "出参：" + param;
+    var result = "出参：" + sum;
     console.log(result);
     return result;
   }
-   
-  // 调用函数并传入参数
-  var input = "Hello World";
-  var output = AA(input);
+  var num1 = 5;
+  var num2 = 10;
+  var output = AA(num1, num2);
   console.log(output);
 }
