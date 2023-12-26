cdb8e118dadf5e3aa4164d8b12c57b09d482632d
1786248714123444.feishu.cn <1786248714123444@feishu.cn>
2023-12-26 08:37:59 +0000

    1 个文件产生了变更，影响了1 个 函数
    更新函数「DD」

--- functions/nodejs/DD/index.js
+++ functions/nodejs/DD/index.js
@@ -14,22 +14,23 @@ module.exports = async function (params, context, logger) {
   // logger.info(`${new Date()} 函数开始执行`);
 
   // 在这里补充业务代码
-
-  function processValue(input) {
+  function addNumbers(num1, num2) {
     // 入参处理
-    console.log('入参: ' + input);
-
-    // 对参数进行处理
-    var processed = input.toUpperCase();
-
+    console.log("入参1：" + num1);
+    console.log("入参2：" + num2);
+  
+    // 执行一些操作
+    var sum = num1 + num2;
+  
     // 出参处理
-    console.log('出参: ' + processed);
-
-    // 返回处理后的参数
-    return processed;
+    var result = "出参：" + sum;
+    console.log(result);
+    return result;
   }
-
-// 调用函数并传入参数
-  var result = processValue('hello');
-  console.log(result);  // 输出: HELLO WORLD
-  }
+  
+  // 调用函数并传入参数
+  var num1 = 5;
+  var num2 = 10;
+  var output = addNumbers(num1, num2);
+  console.log(output);
+}
